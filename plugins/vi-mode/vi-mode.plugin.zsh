b77d13146c596f2bc0ce54ddd1010b3d36c942fc
Jonathan Evans <jonathan.r.h.evans@gmail.com>
2011-11-03 17:35:00 +0000

    Changed to ashleydevs  vi-mode plugin to get history search and jj instead of esc

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -1,22 +1,98 @@
+# ------------------------------------------------------------------------------
+#          FILE:  vi-mode.plugin.zsh
+#   DESCRIPTION:  oh-my-zsh plugin file.
+#        AUTHOR:  Sorin Ionescu (sorin.ionescu@gmail.com)
+#       VERSION:  1.0.3
+# ------------------------------------------------------------------------------
+
+
+# Allow command line editing in an external editor.
+autoload -Uz edit-command-line
+
+# If mode indicator wasn't setup by theme, define a default.
+if [[ "$MODE_INDICATOR" == "" ]]; then
+  MODE_INDICATOR="%{$fg_bold[red]%}<%{$reset_color%}%{$fg[red]%}<<%{$reset_color%}"
+fi
+
+# If I am using vi keys, I want to know what mode I'm currently using.
+# zle-keymap-select is executed every time KEYMAP changes.
+# From http://zshwiki.org/home/examples/zlewidgets
 function zle-line-init zle-keymap-select {
+  if [[ "$KEYMAP" == 'vicmd' ]]; then
+    rprompt_cached="$RPROMPT"
+    RPROMPT="$MODE_INDICATOR"
+  elif [[ -n "$rprompt_cached" ]]; then
+    RPROMPT="$rprompt_cached"
+    rprompt_cached=""
+  fi
   zle reset-prompt
+}
+
+# Accept RETURN in vi command mode.
+function accept_line {
+  if [[ -n "$rprompt_cached" ]]; then
+    RPROMPT="$rprompt_cached"
+    rprompt_cached=""
+  fi
+  builtin zle .accept-line
 }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
+zle -N accept_line
+zle -N edit-command-line
 
-bindkey -v
+# Avoid binding ^J, ^M,  ^C, ^?, ^S, ^Q, etc.
+# bindkey -d # Reset to default. Breaks tab completion. Start tab completing and press tab to move across options and it will kill your terminal.
+bindkey -v # Use vi key bindings.
+bindkey -M vicmd "^M" accept_line # Alow RETURN in vi command.
+bindkey -M vicmd v edit-command-line # ESC-v to edit in an external editor.
 
-# if mode indicator wasn't setup by theme, define default
-if [[ "$MODE_INDICATOR" == "" ]]; then
-  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
+bindkey ' ' magic-space
+bindkey -M vicmd "gg" beginning-of-history
+bindkey -M vicmd "G" end-of-history
+
+# Bind to history substring search plugin if enabled;
+# otherwise, bind to built-in ZSH history search.
+if (( $+plugins[(er)history-substring-search] )); then
+  bindkey -M vicmd "k" history-substring-search-backward
+  bindkey -M vicmd "j" history-substring-search-forward
+else
+  bindkey -M vicmd "k" history-search-backward
+  bindkey -M vicmd "j" history-search-forward
 fi
 
-function vi_mode_prompt_info() {
-  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
-}
+bindkey "^P"          up-line-or-search
+bindkey -M vicmd "k" up-line-or-search
+bindkey -M vicmd "^k" up-line-or-search
+bindkey -M viins "^k" up-line-or-search
+bindkey "^N"          down-line-or-search
+bindkey -M vicmd "j" down-line-or-search
+bindkey -M vicmd "^j" down-line-or-search
+bindkey -M viins "^j" down-line-or-search
 
-# define right prompt, if it wasn't defined by a theme
-if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
-  RPS1='$(vi_mode_prompt_info)'
-fi
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-search-backward
+bindkey -M vicmd "/"  history-incremental-search-backward
+bindkey -M vicmd "?"  history-incremental-search-forward
+
+bindkey -M vicmd "^L" clear-screen
+bindkey -M viins "^L" clear-screen
+
+bindkey -M vicmd "^W" backward-kill-word
+bindkey -M viins "^W" backward-kill-word
+
+bindkey -M vicmd "^A" beginning-of-line
+bindkey -M viins "^A" beginning-of-line
+
+bindkey -M vicmd "^E" end-of-line
+bindkey -M viins "^E" end-of-line
+
+bindkey -M vicmd '^d' delete
+bindkey -M viins '^d' delete
+
+bindkey -M vicmd '^?' backward-delete-char
+bindkey -M viins '^?' backward-delete-char
+
+# 'jj' = ESC
+bindkey -M viins 'jj' vi-cmd-mode
